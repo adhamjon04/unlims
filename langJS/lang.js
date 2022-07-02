0ab2f77cb6060876131fcc241ef773bf2becfbe6
adhamjon04 <https://github.com/adhamjon04>
2022-07-02 23:08:42 +0500

    finished open

--- langJS/lang.js
+++ langJS/lang.js
@@ -491,6 +491,190 @@ const langArr={
 
     },
 
+    /*FAQs */
+
+    "faqsrout":{
+        "uz":"Nima uchun  routerni o'chirib yoqish kerak?",
+        "ru":"Зачем перезагружать роутер?"
+
+    },
+    "xotira":{
+        "uz":"1.Routerning xotira sig'imi ma'lumotlar bilan to'ldirilgan va xatolar ham to'plangan. Bundan tashqari,  marshrutizatorga qancha kop qurilmalar ulangan bo'lsa, xotira tezroq tolib boradi. Bunday holda, Internet tezligi bir necha marta kamayadi va siz qulay ish haqida unutishingiz mumkin. " ,
+        "ru":"1.Емкость памяти маршрутизатора заполнена данными, а также накоплены ошибки. Кроме того,чем больше устройств подключено к маршрутизатору, тем быстрее будет заполняться память. В этом случае, Скорость интернета снижается в несколько раз, и о комфортной работе можно забыть."
+
+    },
+    "electric":{
+        "uz":" 2. Provayderda mumkin bo'lgan qisqa muddatli elektr uzilishi.Bunday holda, yo'riqnoma ishlamay qolishi va tarmoq ulanishi uzilishi mumkin. Natijada - tezlikning pasayishi yoki to'liq yo'qolishi mumkin.",
+        "ru":"2. Возможно кратковременное отключение электроэнергии у провайдера.В этом случае роутер может выйти из строя, а сетевое соединение оборваться. Как следствие-может снизиться или вовсе исчезнуть скорость."
+
+    },
+    "blocks":{
+        "uz":"3. Tarmoq sozlamalarida o'zgarishlar yuz berishi va qurilmangizga << bloklangan >> hududdan IP-manzil tayinlanilishi.Agar siz provayder xizmatlari uchun o'z vaqtida to'lamagan bo'lsangiz, bu sodir bo'lishi mumkin. Ko'pincha, teskari ulanish avtomatik ravishda sodir bo'ladi, ammo istisnolar mavjud.",
+        "ru":"3. В настройках сети могут произойти изменения и вашему устройству будет назначен IP-адрес из << заблокированной >> области. Это может произойти, если вы вовремя не оплатили услуги провайдера.Чаще всего обратное соединение происходит автоматически, но бывают и исключения."
+
+    },
+    "redcolor":{
+        "uz":"Routerdagi 'los' qizil chiroq yonishi  yoki 'pon' miltillashi ?",
+        "ru":"'Los' мигает красным светом или 'pon' мигает на маршрутизаторе ?"
+
+    },
+    "ponlos":{
+        "uz":"Displey tavsifi.Oddiy ish paytida PON indikatori toliq yonib turishi va LOS indikatori o'chirilgan bo'lishi kerak. LOS chirog'i ulanish uchun javobgardir, PON esa provayder tarmog'ida ro'yxatdan o'tish uchun.",
+        "ru":"Описание дисплея.Во время нормальной работы индикатор PON должен мигать, а индикатор LOS-выключаться. Los Flash отвечает за подключение, а PON-за регистрацию в сети провайдера."
+    },
+    "variant":{
+        "uz":" Ko'rsatishning mumkin bo'lgan variantlarini ko'rib chiqing:",
+        "ru":"Рассмотрим возможные варианты рендеринга:"
+
+    },
+    "losponolt":{
+        "uz":"1.LOS va PON ko'rsatkichlari o'chirilgan - OLT ga ulanish yo'q, optik kabel ulagichga ulanmagan yoki kabelda signal yo'q.",
+        "ru":"1.Индикаторы LOS и PON отключены - нет подключения к OLT, оптический кабель не подключен к разъему или нет сигнала на кабеле."
+
+    },
+    "milter":{
+        "uz":" 2.PON tez miltillaydi, LOS o'chirilgan - yo'riqnoma provayder uskunasi bilan aloqa o'rnatishga harakat qilmoqda.",
+        "ru":"2.PON мигает быстро, LOS выключен - роутер пытается установить связь с оборудованием провайдера."
+
+    },
+    "checklos":{
+        "uz":" 3.PON doimiy ravishda yonadi, LOS o'chirilgan - provayder bilan aloqa o'rnatilgan.",
+        "ru":"PON постоянно горит, LOS выключен - связь с провайдером установлена."
+
+    },
+    "yoriq":{
+        "uz":"Ikkala ko'rsatkich ham tez-tez yonib turadi - OLTda identifikatsiyadan o'tishning iloji bo'lmadi,GPON texnologiyasidan foydalangan holda yo'riqnoma noto'g'ri bog'langan. Muammoni hal qilish uchun internet provayderingizga murojaat qiling.",
+        "ru":"Часто загораются оба индикатора - пройти идентификацию в OLT не удалось, Неправильно подключен роутер по технологии GPON.Свяжитесь с вашим интернет-провайдером, чтобы решить проблему."
+
+    },
+    "cabel":{
+        "uz":"4.PON tez yonadi, LOS sekin yonadi - kabelda signal yetarli emas.",
+        "ru":"4.PON загорается быстро, LOS загорается медленно - сигнала в кабеле недостаточно."
+
+    },
+    "losttwosew":{
+        "uz":"5.Ikkala ko'rsatkich ham sekin yonib-o'chib turadi - apparatdagi nosozlik.",
+        "ru":"5.Медленно мигают оба индикатора-аппаратная неисправность."
+
+    },
+    "yechim":{
+        "uz":"Muammolar va yechimlar.",
+        "ru":"Проблемы и решения."
+
+    },
+    "indecator":{
+        "uz":" LOS indikatori provayderga ulanish uchun javobgardir. Agar marshrutizatordagi qizil LOS chirog'i yonib tursa yoki qizil yonib-o'chib tursa,kabel bilan bog'liq muammolar mavjud. Internet va telefoniya ishlamaydi.",
+        "ru":"Индикатор LOS отвечает за подключение к провайдеру. Если на маршрутизаторе мигает красный индикатор питания или мигает красный, есть проблемы с кабелем. Интернет и телефония не работают."
+
+    },
+    "modellers":{
+        "uz":" Routerlarning ba'zi modellarida LOS indikatori taqdim etilmaydi va PON o'z vazifasini bajaradi.",
+        "ru":"В некоторых моделях роутеров индикатор LOS не предусмотрен, а PON выполняет свою функцию."
+
+    },
+    "ponred":{
+        "uz":" Agar PON qizil yonib-o'chsa, bu xuddi shu narsani anglatadi - Internetga ulanish yo'q.",
+        "ru":"Если PON мигает красным, это означает то же самое-Нет подключения к интернету."
+
+    },
+    "wayses":{
+        "uz":" Siz ushbu muammoni quyidagi yo'llar bilan hal qilishga harakat qilishingiz mumkin:",
+        "ru":"Решить данную проблему можно попробовать следующими способами:"
+
+    },
+    "marshrutizzer":{
+        "uz":" Avval marshrutizatordan rozetkaga olib boradigan optik kabelning yaxlitligini tekshirishingiz kerak. Agar siz uni yomon egsangiz yoki mebelni ko'chirsangiz va uni ezib tashlasangiz, u sinishi mumkin. Bundan tashqari, kabel uy hayvonlari tomonidan shikastlanishi mumkin",
+        "ru":"Для начала необходимо проверить целостность оптического кабеля, идущего от роутера к розетке. Если вы плохо согнете его или переместите мебель и раздавите ее, это может сломаться. Кроме того, кабель может быть поврежден домашними животными"
+
+    },
+    "vilkases":{
+        "uz":" Agar kabelda hamma narsa tartibda bo'lsa, vilka router ulagichiga mahkam o'rnashganligini tekshiring. U osilgan emas, mahkam ushlab turishi kerak. Kabelni ajratib oling va qayta ulang. Agar ushbu manipulyatsiyalardan keyin muammo yo'qolmasa, vilkaga chang tushgan bo'lishi mumkin. Agar siz qurilmani joydan boshqa joyga ko'chirsangiz va kabel bir muddat ulanmagan bo'lsa, bu sodir bo'lishi mumkin.  Routerning vilkasini chiqarib oling va avval uni quruq mato bilan artib oling.Agar ifloslanishni olib tashlashning iloji bo'lmasa, u holda spirtli salfetka bilan. Shundan so'ng, kabelni qayta ulang va Internet ishlayotganligini tekshiring. Lazer nurlanishi, hatto ko'zga ko'rinmasa ham, retinaga zarar etkazishi mumkin! Shuning uchun, optik tolali kabelni tozalash bo'yicha barcha manipulyatsiyalar har ikki tomondan lazer nurlanish manbalari o'chirilgan va maxsus himoya ko'zoynaklarida amalga oshirilishi kerak.",
+        "ru":"Если с кабелем все в порядке, проверьте, плотно ли вставлен штекер в разъем роутера. Он должен крепко держаться, а не висеть. Отсоедините и снова подключите кабель. Если после этих манипуляций проблема не исчезла, возможно, в вилку попала пыль. Это может произойти, если вы перемещаете устройство с места на место, а кабель не подключен какое-то время. Выньте штекер маршрутизатора и сначала протрите его сухой тканью.Если нет возможности удалить загрязнения, то спиртовой салфеткой. После этого снова подключите кабель и проверьте, работает ли Интернет. Лазерное излучение, даже невидимое глазу, может повредить сетчатку! Поэтому все манипуляции по очистке оптоволоконного кабеля необходимо проводить в специальных защитных очках, с обеих сторон от источников лазерного излучения."
+
+    },
+    "indikred":{
+        "uz":"Nima uchun  routerni o'chirib yoqish kerak?",
+        "ru":"Зачем перезагружать роутер?"
+
+    },
+    "faqsrouter":{
+        "uz":" Agar indikator qizil yonishda davom etsa, bu liniyada yoki provayder uskunasida muammo.",
+        "ru":"Если индикатор продолжает гореть красным-это проблема на линии или на оборудовании провайдера."
+
+    },
+    "optictola":{
+        "uz":"  Shu munosabat bilan, optik tolali kabellar bilan ishlashda siz quyidagilarga e'tibor berishingiz kerak:",
+        "ru":" В связи с этим при работе с оптоволоконными кабелями следует обратить внимание на:"
+
+    },
+    "shikast":{
+        "uz":" Kabellarning shikastlanishi va sinishidan saqlaning. Uni turtmang.Vilkani chiqarib bo'lgach, uni maxsus himoya qopqog'i bilan yoping. Ushbu qopqoqlarni changdan himoya qilish uchun havo  o'tkazmaydigan idishda saqlash kerak.",
+        "ru":"Избегайте повреждений и поломок кабелей. Не толкай его.После извлечения вилки закройте ее специальным защитным колпачком. Воздух для защиты этих крышек от пыли хранить следует в герметичном контейнере."
+
+    },
+
+    "speedpasteses":{
+        "uz":"Internet tezligi past ?",
+        "ru":"Низкая скорость интернета ?"
+    },
+    "canals":{
+        "uz":" Internet-kanal tezligini tekshirish uchun siz maxsus onlayn xizmatlardan foydalanishingiz mumkin. Muhim! O'lchov natijalari taxminiy va har doim ham aniq bo'lmasligi mumkin.",
+        "ru":"Для проверки скорости интернет-канала можно воспользоваться специальными онлайн-сервисами. Важно! Результаты измерений являются приблизительными и не всегда могут быть точными."
+    },
+    "metrecs":{
+        "uz":"To'g'ri o'lchash va ishonchli qiymatni olish uchun sinov faqat kabel orqali ulangan bitta qurilmadan amalga oshirilishi  kerak (sinov natijalariga ta'sirini istisno qilish uchun boshqa qurilmalarni tarmoqdan uzing).",
+        "ru":"Тестирование должно проводиться только с одного устройства, подключенного кабелем, для точного измерения и получения надежного значения (отключите другие устройства от сети, чтобы исключить влияние на результаты теста)."
+    },
+    "tarmoqs":{
+        "uz":" Routerga Wi-Fi orqali ulangan qurilmalardan sinov qilmang. Agar marshrutizatorga Internet ulanishi tarmoq kabeli (lizing liniyasi) orqali amalga oshirilsa, u holda o'lchash kabel orqali routerga ham ulangan qurilmadan amalga oshirilishi kerak.",
+        "ru":"Не тестируйте устройства, подключенные к маршрутизатору через Wi-Fi. Если подключение интернета к роутеру осуществляется по сетевому кабелю (лизинговой линии, измерение в этом случае должно проводиться с устройства, которое также подключено к маршрутизатору через кабель."
+    },
+    "torrent":{
+        "uz":"Xizmat sinfidan foydalanmang. Agar xizmat ko'rsatish klassi ishlatilsa, u holda qurilma uchun tarmoq tezlatgichi (ppe hardware; hwnat) o'chirib qo'yilgan, chunki tezlatgich trafik ustuvorligi bilan qanday ishlashni bilmaydi. Gaz o'chirilgan bo'lsa, tezlik kamayishi mumkin. Qurilmalar ro'yxati sahifasida “Xizmat sinfi” maydonini sinovdan o'tayotgan qurilmangiz uchun “Foydalanmaslik”ga o'rnating. Sinov paytida Internet-trafikdan (torrentlar, onlayn videolar, o'yinlar va boshqalar) foydalanadigan dasturlarni (ilovalarni) o'chiring.",
+        "ru":"Не используйте класс обслуживания. Если используется сервисный класс, то сетевой ускоритель для устройства (ppe hardware; hwnat)отключается, т. к. акселератор не умеет работать с приоритетом трафика. При выключенном газе скорость может снизиться. На странице списка устройств установите поле “класс обслуживания” на “не использовать”для тестируемого устройства.Отключите программы (приложения), использующие интернет-трафик (торренты, онлайн-видео, игры и т. д.) Во время тестирования."
+    },
+    "sinishcut":{
+        "uz":"Aniqroq va ishonchli natijalarga erishish uchun Internet tezligini turli xizmatlarda 2-3 marta sinab ko'rish va natijalarni solishtirishni tavsiya qilamiz. O'rtacha o'lchov natijasini ko'rib chiqing.",
+        "ru":"Тестирование скорости интернета 2-3 раза на разных сервисах для получения более точных и достоверных результатов и мы рекомендуем сравнить результаты. Рассмотрим средний результат измерения."
+    },
+    "maxsimum":{
+        "uz":" O'lchashda olingan tezlik natijalari taxminiy va sizning tarif rejangiz bo'yicha Internet-kanalning e'lon qilingan maksimal tezligidan past bo'lishi kerak (IPoE uchun taxminan 5-6% va PPPoE, L2TP, PPTP uchun 10% gacha) (bu ma'lumotlarni uzatish uchun ishlatiladigan xizmat ma'lumotlarining miqdori bilan bog'liq).Tekshiruv natijalari provayderning ma'lumotlar kanallari, serverlari, sinov o'tkaziladigan kunning vaqti, ulanish turi va boshqalarga bog'liq ravishda sezilarli darajada farq qilishi mumkin.",
+        "ru":"Результаты скорости, полученные при измерении, являются приблизительными и зависят от заявленной максимальной скорости интернет-канала по вашему тарифному планудолжен быть низким (около 5-6% для IPoE и до 10% для PPPoE, L2TP, PPTP) (используется для передачи данных связано с объемом сервисных данных).Результаты проверки каналов передачи данных провайдера, они могут значительно различаться в зависимости от серверов, времени суток тестирования, типа подключения и т. д."
+    },
+    "misoller":{
+        "uz":" Quyida routerning mahalliy tarmog'idan Internetga ulanish tezligini o'lchash misollari keltirilgan.",
+        "ru":" Ниже приведены примеры измерения скорости интернет-соединения из локальной сети роутера."
+    },
+    "kompbrauzer":{
+        "uz":"Onlayn xizmatlardan foydalangan holda Internet-kanal tezligini sinab ko'rish. Routerga to'g'ridan-to'g'ri ulangan kompyuterda veb-brauzerni ishga tushiring.",
+        "ru":"Тестирование скорости интернет-канала с помощью онлайн-сервисов. Запустите веб-браузер на компьютере, подключенном непосредственно к маршрутизатору."
+    },
+    "famouses":{
+        "uz":"Eng mashhur xizmatlardan biri bu Speedtest.",
+        "ru":"Одним из самых популярных сервисов является Speedtest."
+    },
+    "focuses":{
+        "uz":" Uni ishlatishda ba'zi xususiyatlarga e'tiboringizni qaratmoqchimiz. Odatiy bo'lib, xizmat avtomatik ravishda tezlikni tekshirish uchun optimal serverni tanlaydi. Ammo serverning o'zi joylashgan  joyni hisobga olish muhimdir. Ba'zida xizmat tekshirish uchun serverni noto'g'ri tanlaydi. Xizmat serverni qo'lda belgilash  imkoniyatini beradi. Buning uchun  'Serverni o'zgartirish' havolasini bosing, serverni tanlang va keyin sinovni boshlang.Sinov natijasida siz uchta raqam olasiz: Ping , yuklab olish tezligi  va yuklash tezligi Ping mijozdan yuborilgan maxsus tarmoq paketining tanlangan serverga etib borishi va javobni olishi uchun zarur bo'lgan vaqtni (millisekundlarda) ko'rsatadi (bu vaqt qancha qisqa bo'lsa, shuncha yaxshi). ",
+        "ru":"Мы хотим обратить ваше внимание на некоторые особенности при его использовании. По умолчанию сервис автоматически выбирает оптимальный сервер для проверки скорости. Но сам сервер находится  важно учитывать местоположение. Иногда сервис неправильно выбирает сервер для проверки. Установка сервера сервиса вручную дает возможность. Для этого Нажмите на ссылку ' Изменить сервер', выберите сервер и начните тестирование.В результате теста вы получите три цифры: Ping, скорость загрузки и скорость загрузки ping-это время, необходимое для того, чтобы выделенный сетевой пакет, отправленный от клиента, достиг выбранного сервера и получил ответ (в миллисекундах) показывает (чем короче это время, тем лучше)."
+    },
+    "opendoor":{
+        "uz":"Kiruvchi ulanish tezligi veb-saytlarning qanchalik tez ochilishiga va Internetdan fayllarni yuklab olinishiga ta'sir qiladi. Yuqori ulanish tezligi kompyuteringizdan Internetga ma'lumotlarni uzatishda (masalan, bulutga fotosuratlar yoki fayllarni yuklashda) ishlatiladi.",
+        "ru":"Скорость входящих соединений влияет на то, как быстро открываются веб-сайты и загружаются файлы из интернета. Высокая скорость соединения при передаче данных с компьютера в интернет используется (например, при загрузке фотографий или файлов в облако)."
+    },
+    "":{
+        "uz":"",
+        "ru":""
+    },
+    "":{
+        "uz":"",
+        "ru":""
+    },
+
+
+
+
+
+
 
 
 
